4cc8e403bba56e778e0ba2710e5069278d322b54
TQ White II <tq@justkidding.com>
2015-01-05 11:51:30 -0600

    added github links to explanation

--- server/retrieving.js
+++ server/retrieving.js
@@ -1,4 +1,11 @@
 
+var pingSecretServer=function(){
+	var result=HTTP.call('POST', 'http://localhost:5000/ping')
+	return result;
+}
+
+
+
 Meteor.publish('server.retrieving.findForUser', function() {
 		var selector={ownerId:this.userId};
 		var secrets=Secrets.find(selector);
@@ -12,6 +19,8 @@ Meteor.methods({
 			selector=args.selector;
 		//this should do something good with the secretPassword
 		console.log("Server says, UNUSED SO FAR secretPassword=" + secretPassword);
+		
+		console.dir(pingSecretServer());
 		
 		var secret=Secrets.findOne(selector);
 		return secret;
